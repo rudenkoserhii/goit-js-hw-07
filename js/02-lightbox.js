5863f76c60bec1e7919a5aac9bfea012cb247b9d
RudenkoSerhii <rudenko.sergey@ukr.net>
2022-08-28 17:45:48 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryItemsCollection = createGalleryItemsCollection(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemsCollection);
+
+function createGalleryItemsCollection(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('');
+
+};
+
+
+const lightbox = new SimpleLightbox('.gallery a', { enableKeyboard: "true", captionDelay: "250ms", captions: "true", captionSelector: "img", captionType: "attr", captionsData: "alt" });
